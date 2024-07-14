615cbb0905b71e3beff966b41b87919a61a50ae0
Admin <maayush235@gmail.com>
2024-07-14 15:15:11 +0530

    Radhey Radhey

---
+++ lib/Views/Splash&intro_screen/splashScreen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:space_knowledge/Views/HomeScreen/home_screen.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Start a timer that navigates to the next screen after 3 seconds
+    Timer(const Duration(seconds: 3), () {
+      // Navigator.pushReplacement(
+      //   context,
+      //   MaterialPageRoute(builder: (context) => const HomeScreen()), // Replace with your next screen
+      // );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(),
+            SizedBox(
+                height: 250,
+                width: 250,
+                child: Image.asset(
+                  'assets/images/Rectangle 29.png',
+                  fit: BoxFit.cover,
+                )),
+            Image.asset('assets/images/Vector.png'),
+            SizedBox(
+              height: 10,
+            ),
+            Image.asset('assets/images/Knowledge.png'),
+            Spacer(),
+            Image.asset('assets/images/Button.png'),
+
+            SizedBox(
+              height: 30,
+            )// Add some space below the button
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'Views/HomeScreen/home_screen.dart';
+import 'Views/Splash&intro_screen/splashScreen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -7,14 +10,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
-      home:
+      debugShowCheckedModeBanner: false,
+        home: Splashscreen()
     );
   }
 }
-
-
