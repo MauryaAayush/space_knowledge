e6f0dddfb2a9e531e77e4ff1565dc1016989e51d
Admin <maayush235@gmail.com>
2024-07-14 16:29:08 +0530

    Radhey Radhey

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:space_knowledge/Providers/json_providing.dart';
 import 'Views/Splash&intro_screen/splashScreen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+      MultiProvider(providers: [
+        ChangeNotifierProvider(create: (context) => JsonProvider(),)
+      ],
+        child: const MyApp(),
+      )
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+        debugShowCheckedModeBanner: false,
         home: Splashscreen()
     );
   }
